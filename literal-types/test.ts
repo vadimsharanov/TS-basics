0041440cc972556d0003ec4da9aace69a1a94851
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-10-28 21:12:23 +0300

    classes

---
+++ literal-types/test.ts
@@ -0,0 +1,16 @@
+let something: "test" = "test";
+
+type actionType = "up" | "down";
+
+function performAction(action: actionType | ComplexAction) {
+  switch (action) {
+    case "down":
+      return -1;
+    case "up":
+      return 1;
+  }
+}
+
+interface ComplexAction {
+  s: string;
+}
